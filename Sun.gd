a22cc0b25e86638036126b1a5cff8418d43c2c22
Lilly <47436048+Lilf4@users.noreply.github.com>
2024-07-03 22:35:16 +0200

    imported shaders

---
+++ Sun.gd
@@ -0,0 +1,15 @@
+extends MeshInstance3D
+
+@onready var player = find_parent("Main").find_child("Player")
+@export var offset: Vector3 = Vector3(0,0,2000)
+@export var movPow = 0.05
+var basePos: Vector3
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	position = player.position + offset
+	basePos = position
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position = player.position + offset
